4b249a6bbb7a156ac156cd254bc06862501cc94b
Guillermo Puente <guillermo.ps09@gmail.com>
2021-04-20 10:40:53 -0400

    Updated Storybook

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs/react';
-
-addDecorator(withKnobs({ escapeHTML: false }));
-
-configure(
-  require.context('../src', true, /\.stories\.tsx?$/),
-  module,
-);
--- src/GradientButton/gradient-button.stories.tsx
+++ src/GradientButton/gradient-button.stories.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { select, text } from '@storybook/addon-knobs';
 
-import GradientButton, { GradientButtonProps } from './GradientButton';
+import GradientButton from './index';
 
-export default { title: 'GradientButton' };
+export default {
+  title: 'GradientButton',
+  component: GradientButton,
+};
 
-export const withText = () => {
-  const defaultProps: GradientButtonProps = {
-    children: text('children', 'Some Text'),
-    inclination: select('inclination', [
-      '45deg',
-      '90deg',
-      '180deg',
-    ], '45deg'),
-  };
+const Template = (args) => <GradientButton {...args} />;
 
-  return (
-    <GradientButton {...defaultProps} />
-  );
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Text',
 };
